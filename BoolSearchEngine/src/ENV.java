3a3e50a3f3383564276782e7f6a03abe7a56614c
Jasper Chen <xiubeans@gmail.com>
2013-03-11 23:20:53 -0400

    First commit

---
+++ BoolSearchEngine/src/ENV.java
@@ -0,0 +1,18 @@
+/**
+ * @author Jasper Chen
+ */
+
+/*
+ * Global constants
+ */
+public final class ENV {
+	// Path
+	public final static String BODYDIR = "BodyLists";
+	public final static String TITLEDIR = "TitleLists";
+	// If term is in body field or title field
+	public final static boolean BODY = true;
+	public final static boolean TITLE = false;
+	// If use ranked/unranked retrieval
+	public final static boolean RANKED = true;
+	public final static boolean UNRANKED = false;
+}
